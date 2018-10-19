c1aaa4bdf47260b7e73d15d3ce0b8729d47ab356
Washington Soares <wsoaresfilho@hotmail.com.br>
2018-10-19 20:34:47 -0300

    Adapting the app to work with linux

--- app_oficial/config.py
+++ app_oficial/config.py
@@ -13,17 +13,22 @@ from kivy.graphics import Rectangle
 from kivy.core.window import Window
 
 import cv2 as cv
+import platform
 from myObject import MyObject
 from getobjects import getObjects, clearObjects
 
 # Default configuration values
-CAMERA_DEVICE = 0 # Setset which camera use if there is more than one
+CAMERA_DEVICE = 0 # Sets which camera use if there is more than one
 VIDEO_WIDTH = 960.0
 VIDEO_HEIGHT = 540.0
 ALGOS_LIST = ['SIFT' , 'SURF', 'ORB', 'BRISK', 'KAZE', 'AKAZE']
 FLANN_INDEX_KDITREE = 2
 MIN_MATCH_COUNT = 30
 MATCH_DISTANCE = 0.75
+
+# Insert a necessary parameter when using Linux based system
+if(platform.system() == 'Linux'):
+    CAMERA_DEVICE = CAMERA_DEVICE + cv.CAP_V4L2
 
 # Function that returns the detector and the matcher by the index
 def getDetector(index):
